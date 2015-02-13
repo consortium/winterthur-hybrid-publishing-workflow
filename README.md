adb48fe1033f249e8be1013fc6458e7d3993189f
Furter <loraine.furter@gmail.com>
2015-02-13 14:01:20 +0100

    New modifications, texts and images

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # winterthur-hybrid-publishing-workflow
 Meteor site for Winterthur’s Fotomuseum case study:
+
 http://hybridpublishing.meteor.com/
+
+
 IN PROGRESS
--- hybridpublishing-meteor/hybridpublishing.js
+++ hybridpublishing-meteor/hybridpublishing.js
@@ -1,9 +1,11 @@
 if (Meteor.isClient) {
-  
+// external links
+    
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    
   });
 }
