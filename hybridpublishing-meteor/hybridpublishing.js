320a49a3ad5ac940eaafd98341eaa20b7bd61ada
Eric Schrijver <eric@ericschrijver.nl>
2015-05-10 22:07:04 +0200

    Move to latest Meteor and add Iron Router

--- hybridpublishing-meteor/hybridpublishing.js
+++ hybridpublishing-meteor/hybridpublishing.js
@@ -6,36 +6,13 @@ if (Meteor.isServer) {
 }
 
 if (Meteor.isClient) {
-    // external links
-    var Router = Backbone.Router.extend({
-
-        // quelles routes ont quel function:
-
-        routes: {
-            '': 'staticPage', // --> variable page est undefined
-            ':page': 'staticPage', // --> /about --> variable page est 'about'
-        },
-
-        // le function defini pour montrer le contenu:
-
-        staticPage: function (page) {
-            // if page a un valeur (ex: 'about'), templateName devient ce valeur, si non
-            // templateName devient "main"
-            var templateName = page ? page : "main";
-
-            // on ajoute un variable reutilisable (mais pas utilisée pour l'instant)
-            Session.set('currentPage', page);
-
-            // on demande Meteor de faire un rendu du template correspondent
-            // (dans le fichier html: <template name="about">)
-            var frag = Meteor.render(function () {
-                var i = Template[templateName] ? Template[templateName]() : "";
-                return i;
-            });
-
-        },
+    // initialise router. just one route added
+    Router.route('/', function () {
+        Session.set('currentPage', 'main');
+        this.render('main');
     });
 
+    // external links
     var isExternal = function (href) {
         if (href.indexOf("http") === -1 || href.indexOf(document.location.host) !== -1 || href.indexOf("localhost") !== -1 || href.indexOf("127.0.0.1") !== -1) {
             return false;
@@ -44,14 +21,7 @@ if (Meteor.isClient) {
     };
 
 
-    // initialise le router
-    var app = new Router;
     Meteor.startup(function () {
-        // démarre le router; rendant possible l’utilisation des boutons Back et Forward dans le browser
-        Backbone.history.start({
-            pushState: true
-        });
-
         // une fois que toute la structure de la page est là: action
         $(document).ready(function () {
 
